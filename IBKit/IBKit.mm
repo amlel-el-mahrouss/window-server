a48bf4c1149a4e28cf9aa2cf5b8865fdb99b5396
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-11-04 14:30:53 +0100

    IMP: Clean up IBKit.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ IBKit/IBKit.mm
@@ -0,0 +1,10 @@
+/*
+ * =====================================================================
+ *
+ *            Copyright El Mahrouss Logic, all rights reserved.
+ *
+ * =====================================================================
+ */
+
+#import <IBKit/InterfaceBuilder.h>
+#import <IBKit/DOMRenderer.h>
