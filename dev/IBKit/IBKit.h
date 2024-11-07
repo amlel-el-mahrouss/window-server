d3a6f1e4ec5446cf428305e6f02ad4997988ad86
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-11-07 19:26:58 +0100

    WIP: InterfaceBuilder Graphics Protocol.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- dev/IBKit/IBKit.h
+++ dev/IBKit/IBKit.h
@@ -8,8 +8,4 @@
 
 #pragma once
 
-#define IB_KIT_VERSION_STR "1.0.0"
-#define IB_KIT_VERSION_BCD 0x0100
-
-#define IB_KIT_DEPRECATED __attribute__((deprecated))
-
+#include <IBKit/InterfaceBuilder.h>
--- dev/IBKit/InterfaceBuilder.c
+++ dev/IBKit/InterfaceBuilder.c
@@ -1,9 +1,9 @@
 /*
  * =====================================================================
  *
- *            Copyright (C) 2024, Amlal EL Mahrouss, all rights reserved.
+ *            Copyright (C) 2024, EL Mahrouss Logic, all rights reserved.
  *
  * =====================================================================
  */
 
-#import <IBKit/IBRenderer.h>
+#pragma once
