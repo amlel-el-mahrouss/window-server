1e2a7fbf1c54b830fea9996aa845388815122a4b
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-11-10 09:16:57 +0100

    ADD: Add doc generator to project.

---
+++ make_docs.sh
@@ -0,0 +1,5 @@
+# !/bin/zsh
+
+mkdir -p html
+
+XSDocgen --source ./dev/ --output ./html --undocumented --project-name "ZKA WSKit" --enable-c++ --project-copyright "EL Mahrouss Logic &copy; %Y - All rights Reserved" --project-version "930.2024.1" --company-name "EL Mahrouss Logic"
